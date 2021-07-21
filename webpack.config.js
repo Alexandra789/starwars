5b15d4b0257540009f045350070fa9de8e6ec974
Alexandra789 <zakovinko99@gmail.com>
2021-07-21 16:41:00 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './build/'),
+    filename: 'js/[name].min.js'
+  },
+  devtool: "source-map",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: {removeAll: true},
+            },
+          ],
+        },
+      }),
+      new TerserPlugin({
+        extractComments: true,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        include: path.resolve(__dirname, 'src/js')
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              url: false
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/style.min.css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./assets/images",
+          to: "./img",
+        },
+      ],
+    }),
+  ]
+};
